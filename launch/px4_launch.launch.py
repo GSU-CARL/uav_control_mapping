c42da50fb238627f07abb3d5c53f16e079ef7adc
Norakvitou <norakvitou.s@gmail.com>
2026-05-12 00:36:08 +0700

    create a launch with 3d lidar I already config the 2D lidar into a 3d lidar going to test tmr with fast_lio

--- launch/px4_launch.launch.py
+++ launch/px4_launch.launch.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 import os
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess
+from launch.actions import ExecuteProcess, RegisterEventHandler
+from launch.event_handlers import OnProcessStart
+from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
@@ -13,6 +15,7 @@ def generate_launch_description():
     # Dynamically resolve your package directory to find the world file
     package_dir = get_package_share_directory('uav_control_mapping')
     custom_world_path = os.path.join(package_dir, 'world', 'tugbot_depot.sdf')
+    models_path = os.path.join(package_dir, 'models')
 
     # 1. Start Gazebo Sim (Equivalent to Terminal 1)
     gz_sim_process = ExecuteProcess(
@@ -21,24 +24,69 @@ def generate_launch_description():
         additional_env={
             # Setting the resource path explicitly as in your export command
             'GZ_SIM_RESOURCE_PATH': '/home/fishman/PX4-Autopilot/Tools/simulation/gz/models'
+            ':' + models_path
         }
     )
 
-    # 2. Start PX4 SITL in standalone mode (Equivalent to Terminal 2)
-    px4_sitl_process = ExecuteProcess(
-        cmd=['make px4_sitl gz_x500_depth'],
-        cwd='/home/fishman/PX4-Autopilot',
+    # # 2. Start PX4 SITL in standalone mode (Equivalent to Terminal 2)
+    # px4_sitl_process = ExecuteProcess(
+    #     cmd=['make px4_sitl gz_x500_depth'],
+    #     cwd='/home/fishman/PX4-Autopilot',
+    #     output='screen',
+    #     shell=False,
+    #     additional_env={
+    #         # Passing the standalone flag and world name
+    #         'PX4_GZ_STANDALONE': '1',
+    #         'PX4_GZ_WORLD': 'tugbot_depot',
+    #     }
+    # )
+
+    spawn_entity = Node(
+        package='ros_gz_sim',
+        executable='create',
         output='screen',
-        shell=True,  
-        additional_env={
-            # Passing the standalone flag and world name
-            'PX4_GZ_STANDALONE': '1',
-            'PX4_GZ_WORLD': 'tugbot_depot',
-        }
+        arguments=['-name', 'x500_lidar_3d',
+                   '-file', os.path.join(models_path, 'x500_lidar_3d', 'model.sdf'),
+                   '-x', '0.0', '-y', '0.0', '-z', '0.2']
     )
+
+    px4_sitl_process = ExecuteProcess(
+    cmd=['make', 'px4_sitl', 'gz_x500'],
+    cwd='/home/fishman/PX4-Autopilot',
+    output='screen',
+    shell=False,
+    additional_env={
+        'PX4_GZ_STANDALONE': '1',
+        'PX4_GZ_WORLD': 'tugbot_depot',
+        'PX4_GZ_MODEL_NAME': 'x500_lidar_3d',
+        'GZ_SIM_RESOURCE_PATH': (
+            '/home/fishman/PX4-Autopilot/Tools/simulation/gz/models'
+            ':' + models_path
+        )
+    }
+)
 
     launch_actions.append(gz_sim_process)
-    launch_actions.append(px4_sitl_process)
+    
+    # Wait for Gazebo to start before spawning the drone
+    launch_actions.append(
+        RegisterEventHandler(
+            event_handler=OnProcessStart(
+                target_action=gz_sim_process,
+                on_start=[spawn_entity]
+            )
+        )
+    )
+    
+    # Wait for the model to span before running PX4 SITL to attach to it
+    launch_actions.append(
+        RegisterEventHandler(
+            event_handler=OnProcessStart(
+                target_action=spawn_entity,
+                on_start=[px4_sitl_process]
+            )
+        )
+    )
 
     return LaunchDescription(launch_actions)
 
